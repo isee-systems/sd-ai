79e139c6f06171175737ea4de75c675910237a17
Billy Schoenberg <bschoenberg@iseesystems.com>
2025-01-14 07:02:46 -0500

    update predprey to match spec

--- engines/predprey/engine.js
+++ engines/predprey/engine.js
@@ -22,20 +22,18 @@ class Engine {
                 ],
                 relationships: [
                     {
-                        start: "predator",
-                        end: "prey",
+                        from: "predator",
+                        to: "prey",
                         polarity: "-",
                         reasoning: "As the number of predators increases the number of prey decreases because they get eaten.",
-                        polarityReasoning: "The polarity is - because as predators goes up prey goes down",
-                        relevantText: "There is no relevant text, this is a dummy engine"
+                        polarityReasoning: "The polarity is - because as predators goes up prey goes down"
                     },
                     {
-                        start: "prey",
-                        end: "predator",
+                        from: "prey",
+                        to: "predator",
                         polarity: "+",
                         reasoning: "As the number of prey increases the number of predators increase because they have more food.",
-                        polarityReasoning: "The polarity is + because as prey goes up predators goes up",
-                        relevantText: "There is no relevant text, this is a dummy engine"
+                        polarityReasoning: "The polarity is + because as prey goes up predators goes up"
                     }
                 ]
             }
