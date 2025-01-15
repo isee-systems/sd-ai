14e457f789472430e2004168d9d7c6cde8d14e2b
Billy Schoenberg <bschoenberg@iseesystems.com>
2025-01-14 20:43:48 -0500

    put authentication first - this should resolve issue in production

--- app.js
+++ app.js
@@ -8,15 +8,6 @@ import v1EngineParameters from './routes/v1/engineParameters.js'
 import v1EngineGenerate from './routes/v1/engineGenerate.js'
 
 const app = express()
-
-app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({limit: '50mb', extended: true }));
-
-if (app.get('env') === 'production') {
-  app.set('trust proxy', 1) // trust first proxy
-}
-
 const authenticationKey = process.env.AUTHENTICATION_KEY;
 
 if (authenticationKey) {
@@ -28,6 +19,13 @@ if (authenticationKey) {
   });
 }
 
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({limit: '50mb', extended: true }));
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+}
 
 app.use("/api/v1/initialize", v1Initialize);
 app.use("/api/v1/engines", v1Engines);
