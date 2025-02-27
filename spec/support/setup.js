897406304413b3fa7397edebf06251929086861a
Billy Schoenberg <bschoenberg@iseesystems.com>
2025-02-27 14:40:31 -0500

    increase the timeout to be huge

--- spec/support/setup.js
+++ spec/support/setup.js
@@ -4,7 +4,7 @@ export default function setup() {
   if (reporterAdded)
     return;
 
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 60*1000*10; //10 minutes per test maximum
 
   reporterAdded = true;
   
