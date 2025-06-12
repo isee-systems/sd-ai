bcfc5b9e5459210a298ae073ee51579303047f53
Billy Schoenberg <bschoenberg@iseesystems.com>
2025-06-12 15:33:18 -0400

    let people pass in monster requests

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import v1EngineGenerate from './routes/v1/engineGenerate.js'
 const app = express()
 
 app.use(cors())
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true }));
 
 if (app.get('env') === 'production') {
